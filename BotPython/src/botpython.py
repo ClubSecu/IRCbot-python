61705cb39bdc27c51ae939c1923930a7bebb1b86
canillas <canillas@nuabee-lca>
2015-02-13 14:38:06 +0100

    Initiatilisation

---
+++ BotPython/src/botpython.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "canillas"
+__date__ = "$13 fevr. 2015 14:37:21$"
+
+if __name__ == "__main__":
+    print ("Toto")
