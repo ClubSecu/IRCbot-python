4961949f0344310f058f3173714b014f5c64e9d0
Nomekrax <>
2015-03-06 10:10:03 +0100

    Creation Bot pour les flux reddit uniquement

---
+++ RedditBot/src/redditbot.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "jbussery"
+__date__ = "$6 mars 2015 10:09:20$"
+
+if __name__ == "__main__":
+    print "Hello World"
