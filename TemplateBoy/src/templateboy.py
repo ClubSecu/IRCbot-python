3d8e38b2916242d067f031639347af938302e107
canillas <canillas@NUABEE-LCA>
2015-03-25 17:06:33 +0100

    epurage du code de redditbotV2 pour avoir un bot squelette

--- TemplateBoy/src/templateboy.py
+++ TemplateBoy/src/templateboy.py
@@ -5,3 +5,86 @@
 __author__ = "canillas"
 __date__ = "$25 mars 2015 16:57:05$"
 
+import sys
+import socket
+import string
+import os
+
+#definir la fonction custom ici 
+
+
+
+
+def readline(line,d): #ne gere que PING, PVMSG et JOIN
+    
+    message=""
+    #je traite les  privmsg et les join et les ping FAIRE UN CASE SWITCH
+    
+    #ajout d un champ type toujours present pour faire du traitement sur les 
+    #types d'action 
+    
+    # la taille du tableau est fixe je peux donc forger mon dictionnaire
+    if (line[0]=="PING"):
+        d = dict(id=0,usr ='serv',act='ping',src=line[1])
+        return d 
+    #la taille du tableau est variable     
+    if (line[1]=="PRIVMSG"):
+        user=line[0]
+        action=line[1]
+        source=line[2]
+        #je recupere les informations "fixe" et je les enleve du tableau 
+        line.pop(2)
+        line.pop(1)
+        line.pop(0)
+        # je concatene tout le reste 
+        for word in line:
+            message = message+word+" "
+        #j'enleve le premiere caractere ":" pour avoir un message parsable 
+        message=string.lstrip(message,':')
+        d=dict( id = 1,usr = user, act = action, src = source, msg = message)
+        return d
+    #la taille du tableau est fixe je peux donc forger mon dictionnaire
+    if (line[1]=="JOIN"):
+        d=dict(id=2,usr=line[0],act=line[1],whr=line[2])
+        return d
+
+
+def run():
+    HOST="irc.clubsecu.fr"
+    PORT=6667
+    NICK="Template-Bot" #a renommer 
+    IDENT="BOT"
+    REALNAME="Sp1p3-Bot"
+    readbuffer=""    
+    
+    d=dict()
+    s=socket.socket( )
+    s.connect((HOST, PORT))
+    s.send("NICK %s\r\n" % NICK)
+    s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+    
+   
+    #lecture du ficher contenant les membres du clubs secu et mise de ceux-ci dans un tableau 
+    
+    while 1:
+        readbuffer=readbuffer+s.recv(1024)
+        temp=string.split(readbuffer, "\n")
+        readbuffer=temp.pop( )
+        
+        for line in temp:
+            line=string.rstrip(line)
+            line=string.split(line)
+
+            if(line[0]=="PING"):
+                s.send("PONG %s\r\n" % line[1])
+            s.send("JOIN #resir\r\n")
+          
+     
+            d = readline(line, d)
+            #print(d)
+#inserer la fonction custom ici
+           
+
+if __name__ == "__main__":
+   print(" Template-BOT  au Rapport")
+   run()
