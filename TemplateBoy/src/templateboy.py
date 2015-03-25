9e56e6dc2d167a5d70b53447afa39c31aa00f4fe
canillas <canillas@NUABEE-LCA>
2015-03-25 16:57:58 +0100

    Creation d'un template de bot proc Creation du projet

---
+++ TemplateBoy/src/templateboy.py
@@ -0,0 +1,7 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "canillas"
+__date__ = "$25 mars 2015 16:57:05$"
+
